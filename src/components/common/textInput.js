53f7b2f6c8da2fc0592de17050877d5398e69896
212604718 <anuj.parikh@ge.com>
2017-07-20 00:42:35 -0500

    reusable react component for form input

---
+++ src/components/common/textInput.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React, {PropTypes} from 'react';
+
+const TextInput = (props) => {
+  let wrapperClass = (props.error && props.error.length > 0) ? 'form-group has-error' : 'form-group';
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <div className="field">
+        <input
+          type="text"
+          name={props.name}
+          className="form-control"
+          placeholder={props.placeholder}
+          value={props.value}
+          onChange={props.onChange}
+        />
+        <div className="input">{props.error}</div>
+      </div>
+    </div>
+  );
+};
+
+TextInput.propsType = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+module.exports = TextInput;
